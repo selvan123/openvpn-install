6384068a1403fd763153f417d2071530f2209991
selvan123 <selvans@gmail.com>
2018-04-03 09:06:28 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -156,33 +156,8 @@ if [[ -e /etc/openvpn/server.conf ]]; then
 				#	fi
 					
 					 # Allow traffic initiated from VPN to access LAN
-    iptables -I FORWARD -i tun0 -o eth0 \
-         -s 10.0.0.0/16 -d 192.168.0.0/24 \
-         -m conntrack --ctstate NEW -j ACCEPT
-
-    # Allow traffic initiated from VPN to access "the world"
-    iptables -I FORWARD -i tun0 -o eth1 \
-         -s 10.0.0.0/16 -m conntrack --ctstate NEW -j ACCEPT
-
-    # Allow traffic initiated from LAN to access "the world"
-    iptables -I FORWARD -i eth0 -o eth1 \
-         -s 192.168.0.0/24 -m conntrack --ctstate NEW -j ACCEPT
-
-    # Allow established traffic to pass back and forth
-    iptables -I FORWARD -m conntrack --ctstate RELATED,ESTABLISHED \
-         -j ACCEPT
-
-    # Notice that -I is used, so when listing it (iptables -vxnL) it
-    # will be reversed.  This is intentional in this demonstration.
-
-    # Masquerade traffic from VPN to "the world" -- done in the nat table
-    iptables -t nat -I POSTROUTING -o eth1 \
-          -s 10.0.0.0/16 -j MASQUERADE
-
-    # Masquerade traffic from LAN to "the world"
-    iptables -t nat -I POSTROUTING -o eth1 \
-          -s 192.168.0.0/24 -j MASQUERADE
-					
+    
+    					iptables -t nat -A POSTROUTING -s 10.0.0.0/16 -o eth0 -j MASQUERADE
 					service iptables save
 				fi
 				if hash sestatus 2>/dev/null; then
